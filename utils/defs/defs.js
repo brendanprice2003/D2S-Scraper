f8aa8742a455d153a07b4f6b5a89edd5aceb9039
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2023-05-31 06:31:02 +0100

    Add files via upload

---
+++ utils/defs/defs.js
@@ -0,0 +1,22 @@
+
+export const memshiptypes = {
+    None: 0,
+    TigerXbox: 1,
+    TigerPsn: 2,
+    TigerSteam: 3,
+    TigerBlizzard: 4,
+    TigerStadia: 5,
+    TigerEgs: 6,
+    TigerDemon: 10,
+    BungieNext: 254
+};
+
+export const days = {
+    0: 'sun',
+    1: 'mon',
+    2: 'tues',
+    3: 'wed',
+    4: 'thur',
+    5: 'fri',
+    6: 'sat'
+};
